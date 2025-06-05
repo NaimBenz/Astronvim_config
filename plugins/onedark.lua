2a6ef5d86e2704dd669becea26370dd0a9f46852
naimbenziani <naim599100@gmail.com>
2025-06-05 15:13:40 +0200

    Tokyo night

--- plugins/onedark.lua
+++ plugins/onedark.lua
@@ -1,12 +1,12 @@
-return {
-  -- Thème OneDark.nvim
-  {
-    "navarasu/onedark.nvim",
-    config = function()
-      require("onedark").setup {
-        style = "deep", -- ou "light" selon ta préférence
-      }
-      require("onedark").load()
-    end
-  },
-}
+-- return {
+--   -- Thème OneDark.nvim
+--   {
+--     "navarasu/onedark.nvim",
+--     config = function()
+--       require("onedark").setup {
+--         style = "Deep", -- ou "light" selon ta préférence
+--       }
+--       require("onedark").load()
+--     end
+--   },
+-- }
